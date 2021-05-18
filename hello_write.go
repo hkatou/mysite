5c83c8578df0b75875e7cd39ef9603fe2fe2bfe6
hkatou <hkatou@shimatsu.co.jp>
2021-05-18 14:05:58 +0900

    2021051804

---
+++ hello_write.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/360EntSecGroup-Skylar/excelize/v2"
+
+func main() {
+	var abc int
+	abc = 1234
+	// 新規ファイルを作成 --- (*1)
+	f := excelize.NewFile()
+	// 新規シートを追加 --- (*2)
+	index := f.NewSheet("Sheet2")
+	// セルに値をセット --- (*3)
+	f.SetCellValue("Sheet2", "C2", "こんにちは")
+	// アクティブシートを変更 --- (*4)
+	f.SetActiveSheet(index)
+	// ファイルに保存 --- (*5)
+	f.SaveAs("hello.xlsx")
+}
